9c7cb87599c08fe80fb94b2a148a957492807187
BassHound <basso.andrea.96@gmail.com>
2023-06-07 19:32:36 +0200

    home

---
+++ Client/TawFrontend/src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import {Component, Injectable, OnInit} from '@angular/core';
+import {UserService} from "../User/user.service";
+import {Router} from "@angular/router";
+import {AppComponent} from "../app.component";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent{
+  constructor() {}
+
+}
