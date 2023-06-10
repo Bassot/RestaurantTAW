42a7d59c4f7218a18762ae4f62f2af0295588427
BassHound <basso.andrea.96@gmail.com>
2023-06-10 10:37:53 +0200

    client queue_item updated

--- Client/TawFrontend/src/app/Queue/queue_item.ts
+++ Client/TawFrontend/src/app/Queue/queue_item.ts
@@ -3,7 +3,7 @@ export interface Queue_Item{
   name: string,
   type: string,
   price: number,
-  timestamp: Date,    //to order te queue
+  //timestamp: Date,    //to order te queue, maybe we don't need it on front-end side
   status: string,
   table: number
 }
